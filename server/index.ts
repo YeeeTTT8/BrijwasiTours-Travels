5f358530b250693ef8ec901c5121bc9e0206da9a
YeeeTTT8 <rishitpradeep90@gmail.com>
2025-04-29 22:45:49 +0530

    port

--- server/index.ts
+++ server/index.ts
@@ -73,7 +73,7 @@ app.use((req, res, next) => {
 
   server.listen({
     port,
-    host: "127.0.0.1", // Change from "0.0.0.0"
+    host: "0.0.0.0", // Change from "0.0.0.0"
     reusePort: false,
   }, () => {
     log(`serving on port ${port}`);
